d43154e3a4eb71a30b63839f7ed2fcd4cd35faa6
hyy120120 <hyy120120@gmail.com>
2026-07-22 23:14:48 +0530

    e.g. waiting for bank statement

--- components/calculator/CalculatorClient.jsx
+++ components/calculator/CalculatorClient.jsx
@@ -1948,7 +1948,7 @@ export default function CalculatorClient({ today: initialToday }) {
                                             e.target.value,
                                           )
                                         }
-                                        placeholder="Why is it not available? e.g. waiting for bank statement"
+                                        placeholder="Why is it not available?  "
                                       />
                                     </Field>
                                   ) : (
